fa8aae68ed03bf7ed22c511e7297d138943b1539
Eiden O. Garcia Torres <30579580+EidenGarcia@users.noreply.github.com>
2021-07-12 04:30:49 -0700

    Add files via upload

---
+++ angle_speed(.3).py
@@ -0,0 +1,44 @@
+from jetbot import Robot
+import time
+
+robot = Robot()
+
+#45 degree
+robot.left(0.3)
+time.sleep(0.2)
+robot.stop()
+
+#90 degree
+robot.left(0.3)
+time.sleep(0.325)
+robot.stop()
+
+#135 degree
+robot.left(0.3)
+time.sleep(0.5)
+robot.stop()
+
+#180 degree
+robot.left(0.3)
+time.sleep(0.625)
+robot.stop()
+
+#225 degree
+robot.left(0.3)
+time.sleep(0.8)
+robot.stop()
+
+#270 degree
+robot.left(0.3)
+time.sleep(0.925)
+robot.stop()
+
+#315 degree
+robot.left(0.3)
+time.sleep(1.0)
+robot.stop()
+
+#360 degree
+robot.left(0.3)
+time.sleep(1.22)
+robot.stop()
