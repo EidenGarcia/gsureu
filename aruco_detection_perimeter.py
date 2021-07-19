e733eec0028396bfc5639dff612e05e0d8cae8c2
Eiden O. Garcia Torres <30579580+EidenGarcia@users.noreply.github.com>
2021-07-19 12:34:53 -0700

    Create aruco_detection_perimeter.py

---
+++ aruco_detection_perimeter.py
@@ -0,0 +1,64 @@
+from jetbot import Robot
+import time
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+robot = Robot()
+cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink' , cv2.CAP_GSTREAMER)  # Get the camera source
+
+while True:
+    ret, frame = cap.read()
+    # operations on the frame come here
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_100)  # Use 5x5 dictionary to find markers
+    parameters = aruco.DetectorParameters_create()  # Marker detection parameters
+    # lists of ids and the corners beloning to each id
+	#ids = np.array([1])
+    corners, ids, rejected_img_points = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+	#print(ids)
+    
+
+    if corners:
+        for id in ids:
+            if id == 2:
+                print("90-degree angle left")
+                robot.left(0.3)
+                time.sleep(0.325)
+                robot.stop()
+                break
+    
+    if corners:
+        for id in ids:
+            if id == 8:
+                print("45-degree angle left")
+                robot.left(0.3)
+                time.sleep(0.2)
+                robot.stop()
+                break
+
+    if corners:
+        for id in ids:
+            if id == 7:
+                print("90-degree angle left")
+                robot.left(0.3)
+                time.sleep(0.325)
+                robot.stop()
+                break
+    else:
+        robot.forward(0.2)
+        time.sleep(0.001)
+        
+            
+                
+				
+    frame = aruco.drawDetectedMarkers(frame, corners, ids)
+
+	# Display the resulting frame
+    cv2.imshow('frame', frame)
+    # Wait 3 milisecoonds for an interaction. Check the key and do the corresponding job.
+    key = cv2.waitKey(3) & 0xFF
+    if key == ord('q'):  # Quit
+        break
+    
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
